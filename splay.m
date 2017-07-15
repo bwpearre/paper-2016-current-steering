80667e5d681007993199dae2f34f1eb464459853
Ben <bwpearre@gmail.com>
2017-07-14 21:15:55 -0400

    Big rewrite of Introduction. Fix .gitignore. Modify splay analysis.

--- splay.m
+++ splay.m
@@ -1,5 +1,7 @@
 clear;
 
+show_all_charts = false;
+
 % Read the data file
 fid = fopen('splay.txt', 'r');
 header = textscan(fid, '%s %s %s %s %s %s %s\n', 1, 'Delimiter', '\t');
@@ -49,55 +51,59 @@ end
 
 figure(126);
 
-%% Pie chart
-subplot(3,3,1);
-p = pie(counts, key_legend);
-set(gca, 'ColorOrder', colours);
-title('Splay reliability');
-
-%% Mean inter-electrode distance
-subplot(3,3,2);
-for i = 1:length(key_ind)
-    means(i) = mean(data(indices{i},4));
-    stds(i) = std(data(indices{i},4));
+if show_all_charts
+    %% Pie chart
+    subplot(3,3,1);
+    p = pie(counts, key_legend);
+    set(gca, 'ColorOrder', colours);
+    title('Splay reliability');
+    
+    %% Mean inter-electrode distance
+    subplot(3,3,2);
+    for i = 1:length(key_ind)
+        means(i) = mean(data(indices{i},4));
+        stds(i) = std(data(indices{i},4));
+    end
+    h = bar(key_ind, means, 'FaceColor', 0.5*[1 1 1]);
+    
+    hold on;
+    errorbar(key_ind, means, stds, 'k. ');
+    hold off;
+    %set(gca, 'XTickLabels', key_legend);
+    xticklabel_rotate(key_ind, 30, key_legend);
+    ylabel('Distance (\mu m)');
+    title('Mean inter-electrode distance');
+    
+    
+    %% Inter-electrode distance
+    subplot(3,3,3);
+    for i = 1:length(key_ind)
+        means(i) = mean(data(indices{i},6));
+        stds(i) = std(data(indices{i},6));
+    end
+    h = bar(key_ind, means, 'FaceColor', 0.5*[1 1 1]);
+    hold on;
+    errorbar(key_ind, means, stds, 'k. ');
+    hold off;
+    foo = get(gca, 'YLim');
+    set(gca, 'YLim', [0 foo(2)]);
+    %set(gca, 'XTickLabels', key_legend);
+    xticklabel_rotate(key_ind, 30, key_legend);
+    ylabel('Distance (\mu m)');
+    title('Maximum inter-electrode distance');
+    
+    
+    %% Electrode distances per bundle
+    subplot(3,3,[4:9]);
+else
+    subplot(1,1,1);
 end
-h = bar(key_ind, means, 'FaceColor', 0.5*[1 1 1]);
-
-hold on;
-errorbar(key_ind, means, stds, 'k. ');
-hold off;
-%set(gca, 'XTickLabels', key_legend);
-xticklabel_rotate(key_ind, 30, key_legend);
-ylabel('Distance (\mu m)');
-title('Mean inter-electrode distance');
-
-
-%% Inter-electrode distance
-subplot(3,3,3);
-for i = 1:length(key_ind)
-    means(i) = mean(data(indices{i},6));
-    stds(i) = std(data(indices{i},6));
-end
-h = bar(key_ind, means, 'FaceColor', 0.5*[1 1 1]);
-hold on;
-errorbar(key_ind, means, stds, 'k. ');
-hold off;
-foo = get(gca, 'YLim');
-set(gca, 'YLim', [0 foo(2)]);
-%set(gca, 'XTickLabels', key_legend);
-xticklabel_rotate(key_ind, 30, key_legend);
-ylabel('Distance (\mu m)');
-title('Maximum inter-electrode distance');
-
-
-%% Electrode distances per bundle
-subplot(3,3,[4:9]);
 cla;
 hold on;
 markers = {'^', 'd', 's'};
 handles = [];
 for i = 1:length(key_ind)
-    handles(i) = scatter(data(indices{i},4), data(indices{i},6), [], colours(i,:), markers{i});
+    handles(i) = scatter(data(indices{i},4), data(indices{i},6), [], colours(i,:), markers{i}, 'filled');
 end
 hold off;
 xlabel('Average inter-electrode distance per bundle (\mu m)');
